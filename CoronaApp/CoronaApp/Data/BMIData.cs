22f501a3f583e55312489fec661eb507ed654db2
Andrei Iorga <Iorga_A@tegut.com>
2020-03-12 11:52:05 +0100

    form, external razor lib etc

---
+++ CoronaApp/CoronaApp/Data/BMIData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronaApp.Data
+{
+    public class BMIData
+    {
+        public int Height { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ CoronaApp/CoronaApp/Pages/Bmi.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronaApp.Pages
+{
+    public partial class Bmi
+    {
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
